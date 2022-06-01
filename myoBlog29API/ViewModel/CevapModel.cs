928e30c5b1c86a84ac04929dc4759fe0934d97a5
Ozan Ali Yeşil <ozanaliyesil@icloud.com>
2022-06-01 14:24:21 +0300

    Add project files.

---
+++ myoBlog29API/ViewModel/CevapModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myoBlog29API.ViewModel
+{
+    public class CevapModel
+    {
+        public int CevapId { get; set; }
+        public Nullable<int> UyeId { get; set; }
+        public Nullable<int> MakaleId { get; set; }
+        public Nullable<int> Cevap1 { get; set; }
+        public int KatMakaleSay { get; set; }
+    }
+}
